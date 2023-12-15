255dc812d0c70a337de565a34bd241546d49af36
Shirin Kydralieva <149779032+shirinkydralieva@users.noreply.github.com>
2023-12-15 20:07:55 +0600

    solved conflicts

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,41 @@
+
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Amir(21343,234 ,2));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите  первое число: ");
+        int scanNum1 = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int scanNum2 = scanner.nextInt();
+        System.out.println("Выберите операцию:\n 1.+\n 2.-\n 3.*\n 4./ ");
+
+        int choose = scanner.nextInt();
+
+
     }
 
-    public  static double Amir(double num1, double num2, int action) {
+    public static double sumAndDif(double firstNumber, double secondNumber, int action) {
+        switch (action) {
+            case 1:
+                return firstNumber + secondNumber;
+            case 2:
+                return firstNumber - secondNumber;
+            default:
+                return 0;
+        }
+    }
+
+    public static double divAndMulti(double num1, double num2, int action) {
         switch (action) {
             case 1:
                 return num1 * num2;
             case 2:
                 return num1 / num2;
-                    default:
-                        return 0;
+            default:
+                return 0;
 
-                }
         }
     }
+}
