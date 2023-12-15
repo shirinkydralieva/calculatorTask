b474317011ccf0cdfe1eec569c2cd3afc8b9ec05
UseinkyzyMeerim <149767355+UseinkyzyMeerim@users.noreply.github.com>
2023-12-15 20:12:16 +0600

    trysrye

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public static void result() {
+        int num = 0;
+        while (num != 0) {
+
+            switch (num) {
+                case 1:
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println();
+                    break;
+                case 0:
+                    System.out.println("YOU'RE OUT");
+                    break;
+                default:
+                    System.out.println("ERROR, TRY AGAIN!");
+                    break;
+            }
+        }
+
 
     }
 }
