57f8d839d3308719cea3e6e24e9f0888fb2f51c8
Bekzat-Y <bekayrysbaev551@gmail.com>
2023-12-15 19:51:48 +0600

    scan

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+// then press Enter. You can now see whitespace characters in your code\
+//
+//
+//
 
-    }
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите  первое число: ");
+        int scanNum1 = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int scanNum2 = scanner.nextInt();
+        System.out.println("Выберите операцию:\n 1.+\n 2.-\n 3.*\n 4./ ");
+
+        int choose = scanner.nextInt();
+
+
+}
 }
