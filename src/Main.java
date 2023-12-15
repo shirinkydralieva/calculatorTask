045ec854aeff56e6a868b4733d5b8b8138cbe37f
UseinkyzyMeerim <149767355+UseinkyzyMeerim@users.noreply.github.com>
2023-12-15 20:13:47 +0600

    trysrye

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    }
-
-    public static void result() {
         int num = 0;
         while (num != 0) {
 
